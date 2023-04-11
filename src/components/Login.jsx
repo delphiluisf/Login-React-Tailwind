f0e7461a0e10b0bdd5b6ae2aa263390fe986b6c8
delphiluisf <69805371+delphiluisf@users.noreply.github.com>
2023-04-11 11:07:00 -0500

    UI ok, listo para agregar interaccion  nueva rama

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,6 +41,11 @@ export default function Login() {
                     <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>
                         Sing In
                     </button>
+                    <div className='flex after:justify-center'>
+                        <p className='text-white text-sm'>
+                            By Luis Fernando Orjuela
+                        </p>
+                    </div>
                 </form>
             </div>
         </div>
